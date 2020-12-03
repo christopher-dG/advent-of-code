e28c9e8a4ce1018ac583da102945fee6d17dd0e7
Chris de Graaf <me@cdg.dev>
2020-12-02 19:25:59 -0600

    Format

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,13 @@
 open Core
-
 open Lib
 
 let () =
   let argv = Sys.get_argv () in
   let day = int_of_string argv.(1) in
-  let f = match day with
-  | 1 ->
-     Day1.run
-  | 2 ->
-     Day2.run
-  | _ ->
-     raise (Invalid_argument "Unsupported day") in
+  let f =
+    match day with
+    | 1 -> Day1.run
+    | 2 -> Day2.run
+    | _ -> raise (Invalid_argument "Unsupported day")
+  in
   f ()
--- lib/util.ml
+++ lib/util.ml
@@ -1,10 +1,5 @@
 open Core
 
-let read_lines path =
-  path
-  |> In_channel.read_lines
+let read_lines path = path |> In_channel.read_lines
 
-let read_ints path =
-  path
-  |> read_lines
-  |> List.map ~f:int_of_string
+let read_ints path = path |> read_lines |> List.map ~f:int_of_string
