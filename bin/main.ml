dc7d9e1ba7bbdb233ba5402566860dab64e87c9c
Chris de Graaf <me@cdg.dev>
2020-12-03 00:01:10 -0600

    Two digits

--- bin/main.ml
+++ bin/main.ml
@@ -6,9 +6,9 @@ let () =
   let day = int_of_string argv.(1) in
   let f =
     match day with
-    | 1 -> Day1.run
-    | 2 -> Day2.run
-    | 3 -> Day3.run
+    | 1 -> Day01.run
+    | 2 -> Day02.run
+    | 3 -> Day03.run
     | _ -> raise (Invalid_argument "Unsupported day")
   in
   f ()
