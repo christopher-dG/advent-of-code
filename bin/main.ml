5e5de9b7ac44f900968789bdd3183c8b4ea45ba4
Chris de Graaf <me@cdg.dev>
2020-12-02 23:58:37 -0600

    Day 3

--- bin/main.ml
+++ bin/main.ml
@@ -8,6 +8,7 @@ let () =
     match day with
     | 1 -> Day1.run
     | 2 -> Day2.run
+    | 3 -> Day3.run
     | _ -> raise (Invalid_argument "Unsupported day")
   in
   f ()
---
+++ lib/day3.ml
@@ -0,0 +1,51 @@
+open Core
+open Util
+
+let build_grid lines =
+  lines |> Array.of_list
+  |> Array.map ~f:(fun line ->
+         line |> String.to_array |> Array.map ~f:(Char.equal '#'))
+
+let rec traverse_rec grid ~x ~y ~xd ~yd ~trees =
+  if y >= Array.length grid then trees
+  else
+    let row = grid.(y) in
+    let loc = grid.(y).(x mod Array.length row) in
+    let trees = if loc then trees + 1 else trees in
+    traverse_rec grid ~x:(x + xd) ~y:(y + yd) ~xd ~yd ~trees
+
+let traverse grid ~x ~y = traverse_rec grid ~x:0 ~y:0 ~trees:0 ~xd:x ~yd:y
+
+let part1 lines = lines |> build_grid |> traverse ~x:3 ~y:1
+
+let part2 lines =
+  let grid = build_grid lines in
+  [ (1, 1); (3, 1); (5, 1); (7, 1); (1, 2) ]
+  |> List.map ~f:(fun (x, y) -> traverse grid ~x ~y)
+  |> List.reduce_exn ~f:(fun x y -> x * y)
+
+let run () =
+  let lines = read_lines "input/3.txt" in
+  let sol1 = part1 lines in
+  let sol2 = part2 lines in
+  printf "Part 1: %d\nPart 2: %d\n" sol1 sol2
+
+let%test "examples" =
+  let lines =
+    "..##.......\n\
+     #...#...#..\n\
+     .#....#..#.\n\
+     ..#.#...#.#\n\
+     .#...##..#.\n\
+     ..#.##.....\n\
+     .#.#.#....#\n\
+     .#........#\n\
+     #.##...#...\n\
+     #...##....#\n\
+     .#..#...#.#" |> String.strip |> String.split ~on:'\n'
+  in
+  part1 lines = 7 && part2 lines = 336
+
+let%test "input" =
+  let lines = read_lines "../input/3.txt" in
+  part1 lines = 276 && part2 lines = 7812180000
